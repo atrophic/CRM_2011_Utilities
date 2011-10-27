44caee7dd3fcbe71a04b314f4c343a60ab543f3c
Caleb Harrelson <charrelson@sincerasolutions.com>
2011-10-27 15:48:01 -0500

    Added license info.

--- countries_and_states.js
+++ countries_and_states.js
@@ -1,4 +1,33 @@
-/// <reference path="new_common.js" />
+/*
+
+Copyright (C) 2011 by Sincera Solutions, Inc.
+
+If you find this script helpful to you, and especially if you end up using
+it, we'd appreciate a like, vote up, heart, or whatever else is appropriate
+on the site that you found the code.  Failing all that, drop us a line at
+support@sincerasolutions.com to let us know you found it useful.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
+
+
 ///<reference path="IntelliSense\XrmPage-vsdoc.js"/>
 
 function wireUpCountryAndStateFields(countryFieldName, stateFieldName) {
